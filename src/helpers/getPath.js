a9b6148636c7542db8849a85be5c4c58884638b1
solidados <geekbrains.pk@internet.ru>
2024-02-01 16:41:54 +0400

    feat: change structure; prepare lib with helpers and notifiers

--- src/helpers/getPath.js
+++
@@ -1,8 +0,0 @@
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-// path to current file
-export const getFilename = fileURLToPath(import.meta.url)
-
-// path to current directory
-export const getDirname = () => dirname(getFilename)
