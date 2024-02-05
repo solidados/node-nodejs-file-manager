ec46468d9ea9c38c6fdac515cabdcd32a07737d3
solidados <geekbrains.pk@internet.ru>
2024-02-05 12:53:22 +0400

    chore: update notifiers

--- src/helpers/notifiers.js
+++ src/helpers/notifiers.js
@@ -2,7 +2,7 @@ import { EOL } from 'node:os'
 import { colors } from "./messageColors.js";
 
 const notifiers = {
-  failed: () => console.log(`${colors.red}FS operation failed${colors.reset}${EOL}`),
+  failed: () => console.log(`${colors.red}Operation failed${colors.reset}${EOL}`),
   greeting: (username) => console.log(`${EOL}${colors.green}Welcome to the File Manager, ${username}!${colors.reset}`),
   goodbye: (username) => console.log(`${EOL}${colors.yellow}Thank you for using File Manager, ${username}, goodbye!${colors.reset}${EOL}`),
   invalid: () => console.log(`${colors.red}Invalid command${colors.reset}${EOL}`),
